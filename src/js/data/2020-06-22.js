489ac885b9f26e52995da8e5febbc25ae1e5826b
Damian <dxn816@gmail.com>
2020-06-23 22:47:23 -0500

    Added international Hololives and Holostars

--- src/js/data/2020-06-22.js
+++ src/js/data/2020-06-22.js
@@ -13,14 +13,22 @@ dataSet[dataSetVersion].options = [
         { name: "Hololive 2nd gen", key: "gen2" },
         { name: "Hololive 3rd gen", key: "gen3" },
         { name: "Hololive 4th gen", key: "gen4" },
-        { name: "Hololive GAMERS", key: "gamers" }
+        { name: "Hololive GAMERS", key: "gamers" },
+        { name: "Hololive China", key: "china" },
+        { name: "Hololive Indonesia", key: "id" },
+        { name: "Holostars", key: "stars"}
     ]
+    },
+  {
+    name: "Remove non-JP Holos",
+    key: "notjphololive",
+    tooltip: "Check this to remove all non-JP Hololive characters. HyperPlease.",
   },
-  //{
-  //  name: "Remove Non-Girls",
-  //  key: "notgirl",
-  //  tooltip: "Check this to remove all non-female characters."
-  //}
+  {
+    name: "Remove Non-Girls",
+    key: "notgirl",
+    tooltip: "Check this to remove all non-female characters."
+  }
 ];
 
 dataSet[dataSetVersion].characterData = [
@@ -218,6 +226,177 @@ dataSet[dataSetVersion].characterData = [
         img: "wdZUtEg.png",
         opts: {
             generation: ["gen4"],
+        }
+    },
+    {
+        name: "Yogiri",
+        img: "8uyCIbp.png",
+        opts: {
+            generation: ["china"],
+            notjphololive: true
+        }
+    },
+    {
+    name: "Civia",
+    img: "TQwYoKA.png",
+        opts: {
+            generation: ["china"],
+            notjphololive: true
+        }
+    },
+    {
+    name: "Spade Echo",
+        img: "HbebtrC.png",
+        opts: {
+            generation: ["china"],
+            notjphololive: true
+        }
+    },
+    {
+    name: "Doris",
+        img: "ZXjHi8f.png",
+        opts: {
+            generation: ["china"],
+            notjphololive: true
+        }
+    },
+    {
+    name: "Artia",
+        img: "PILecug.png",
+        opts: {
+            generation: ["china"],
+            notjphololive: true
+        }
+    },
+    {
+    name: "Rosalyn",
+        img: "dpMTyGF.png",
+        opts: {
+            generation: ["china"],
+            notjphololive: true
+        }
+    },
+    {
+    name: "Ayunda Risu",
+        img: "BhRbyEU.png",
+        opts: {
+            generation: ["id"],
+            notjphololive: true
+        }
+    },
+    {
+    name: "Moona Hoshinova",
+        img: "u9NwKye.png",
+        opts: {
+            generation: ["id"],
+            notjphololive: true
+        }
+    },
+    {
+    name: "Airani Iofifteen",
+        img: "hGAkQXZ.png",
+        opts: {
+            generation: ["id"],
+            notjphololive: true
+        }
+    },
+    {
+    name: "Hanasaki Miyabi",
+        img: "JmjAl51.png",
+        opts: {
+            generation: ["stars"],
+            notgirl: true,
+            notjphololive: true
+        }
+    },
+    {
+    name: "Kagami Kira",
+        img: "6JI1hQz.png",
+        opts: {
+            generation: ["stars"],
+            notgirl: true,
+            notjphololive: true
+        }
+    },
+    {
+    name: "Kanade Izuru",
+        img: "as6Tj8A.png",
+        opts: {
+            generation: ["stars"],
+            notgirl: true,
+            notjphololive: true
+        }
+    },
+    {
+    name: "Arurandeisu",
+        img: "tCrsBrq.png",
+        opts: {
+            generation: ["stars"],
+            notgirl: true,
+            notjphololive: true
+        }
+    },
+    {
+    name: "Rikka",
+        img: "31J4UYx.png",
+        opts: {
+            generation: ["stars"],
+            notgirl: true,
+            notjphololive: true
+        }
+    },
+    {
+    name: "Astel Leda",
+        img: "LBBvcPs.png",
+        opts: {
+            generation: ["stars"],
+            notgirl: true,
+            notjphololive: true
+        }
+    },
+    {
+    name: "Kishido Temma",
+        img: "Dw68kL3.png",
+        opts: {
+            generation: ["stars"],
+            notgirl: true,
+            notjphololive: true
+        }
+    },
+    {
+    name: "Yukoku Roberu",
+        img: "Zwtz59X.png",
+        opts: {
+            generation: ["stars"],
+            notgirl: true,
+            notjphololive: true
+        }
+    },
+    {
+    name: "Tsukishita Kaoru",
+        img: "MiCGUjf.png",
+        opts: {
+            generation: ["stars"],
+            notgirl: true,
+            notjphololive: true
+        }
+    },
+    {
+    name: "Kageyama Shien",
+        img: "LHG6bKg.png",
+        opts: {
+            generation: ["stars"],
+            notgirl: true,
+            notjphololive: true
+        }
+    },
+    {
+    name: "Aragami Oga",
+        img: "CgiQ2yR.png",
+        opts: {
+            generation: ["stars"],
+            notgirl: true,
+            notjphololive: true
         }
     },
 ];
